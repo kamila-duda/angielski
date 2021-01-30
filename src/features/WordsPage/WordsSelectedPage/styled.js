5290bc32785481d82c4ee91d5e6b515828c2d275
Kamila Duda <kakus86@o2.pl>
2021-01-30 21:45:18 +0100

    added hover effect to words tile

--- src/features/WordsPage/WordsSelectedPage/styled.js
+++ src/features/WordsPage/WordsSelectedPage/styled.js
@@ -3,28 +3,32 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledTitle = styled.h2`
-font-size: 20px;
+  font-size: 20px;
 `;
 export const StyledPolishWord = styled.h3`
-font-size: 15px;
-color: ${({theme})=>theme.color.middle};
+  font-size: 15px;
+  color: ${({ theme }) => theme.color.middle};
 `;
 export const StyledLink = styled(Link)`
-    text-decoration: none;
+  text-decoration: none;
 `;
 export const StyledAnchor = styled.a`
-text-decoration: none;
-position: relative;
-text-align: center;
-cursor: pointer;
-margin: 8px;
-border-radius: 15px;
-box-shadow: 0px 0px 5px 0px #000;
+  text-decoration: none;
+  position: relative;
+  text-align: center;
+  cursor: pointer;
+  margin: 8px;
+  border-radius: 15px;
+  box-shadow: 0px 0px 5px 0px #000;
+  &:hover {
+    transform: scaleX(1.01);
+    box-shadow: 0px 0px 8px 0px #000;
+  }
 `;
 export const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-color: ${({theme})=>theme.color.middle};
-font-size: 30px;
-position: absolute;
-right: 5px;
-bottom: 5px;
+  color: ${({ theme }) => theme.color.middle};
+  font-size: 30px;
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
 `;
