4c3719f1de6d3f076349fd622b89e8143bd3f2ad
Kamila Duda <kakus86@o2.pl>
2021-01-30 21:44:46 +0100

    formatted code

--- src/common/ErrorPage/index.js
+++ src/common/ErrorPage/index.js
@@ -3,19 +3,19 @@ import { useDispatch } from "react-redux";
 import { resetTest } from "../../features/testSlice";
 import { toHomePage } from "../../routes";
 import Container from "../Container";
-import { StyledText, StyledLink } from "./styled"
+import { StyledText, StyledLink } from "./styled";
 
 const ErrorPage = () => {
-    
- const dispatch = useDispatch();
-    return (
-        <Container column={true}>
-            <StyledText>Ups, coś poszło nie tak.</StyledText>
-            <StyledText>Wróć na stronę główną:</StyledText>
-            <StyledLink to={toHomePage()} onClick={dispatch(resetTest())}>Powrót na stronę główną</StyledLink>
-        </Container>
-    )
-
+  const dispatch = useDispatch();
+  return (
+    <Container column={true}>
+      <StyledText>Ups, coś poszło nie tak.</StyledText>
+      <StyledText>Wróć na stronę główną:</StyledText>
+      <StyledLink to={toHomePage()} onClick={dispatch(resetTest())}>
+        Powrót na stronę główną
+      </StyledLink>
+    </Container>
+  );
 };
 
 export default ErrorPage;
