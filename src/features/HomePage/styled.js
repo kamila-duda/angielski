da549b010cc5cc8b6c08d38530f36069f6878d78
Kamila Duda <kakus86@o2.pl>
2021-02-04 21:10:40 +0100

    added horizontal line in Home Page

--- src/features/HomePage/styled.js
+++ src/features/HomePage/styled.js
@@ -5,9 +5,19 @@ export const StyledTitle = styled.h2`
   font-weight: bold;
   text-transform: uppercase;
   font-size: 34px;
-  width: 80%;
+  width: 70%;
   display: flex;
   text-align: center;
+  position: relative;
+  &::after{
+content: "";
+position: absolute;
+width: 100%;
+height: 2px;
+background-color: ${({theme})=>theme.color.middle};
+left: 0;
+bottom: -20px;
+  }
   @media (max-width: ${({ theme }) => theme.breakpoint.s}) {
     font-size: 28px;
   }
